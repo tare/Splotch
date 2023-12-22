ccaf5119971cd9eaa93781011e466ee3870ef0ce
Tarmo Äijö <506537+tare@users.noreply.github.com>
2023-12-22 08:08:23 -0500

    Add run_nuts and run_svi tests

--- tests/test_splotch.py
+++ tests/test_splotch.py
@@ -1,4 +1,5 @@
 """test_splotch.py."""
+import os
 import re
 from io import BytesIO
 
@@ -7,6 +8,7 @@ import pandas as pd
 import pytest
 import scipy.stats
 from jax import random
+from splotch.inference import run_nuts, run_svi
 from splotch.registration import register
 from splotch.utils import (
     SplotchInputData,
@@ -20,6 +22,9 @@ from splotch.utils import (
     savagedickey,
     separate_tissue_sections,
 )
+
+# this is needed for the tests using pmap
+os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=4"
 
 
 @pytest.fixture(scope="session")
@@ -511,6 +516,89 @@ def splotchinputdata_registration() -> SplotchInputData:
     )
 
 
+@pytest.fixture()
+def splotchinputdata_inference() -> SplotchInputData:
+    """SplotchInputData."""
+    count_files = ["count_file_1.tsv", "count_file_2.tsv"]
+    annotation_files = ["annotation_file_1.tsv", "annotation_file_2.tsv"]
+
+    metadata_df = pd.DataFrame(
+        {
+            "level_1": ["cond1", "cond2"],
+            "count_file": count_files,
+            "annotation_file": annotation_files,
+            "image_file": ["image_1.jpeg", "image_2.jpeg"],
+        }
+    )
+    counts_dfs = [
+        pd.DataFrame(
+            {"1_1": [2, 1], "2_2": [5, 10], "3_3": [1, 1], "4_4": [1, 2]},
+            index=["g1", "g2"],
+        ),
+        pd.DataFrame(
+            {
+                "8_8": [10, 5],
+                "7_7": [5, 1],
+                "6_6": [3, 1],
+                "5_5": [1, 4],
+                "11_1": [10, 5],
+                "11_0": [5, 1],
+                "11_-1": [3, 1],
+                "11_-2": [1, 4],
+            },
+            index=["g1", "g2"],
+        ),
+    ]
+
+    for filename, count_df in zip(count_files, counts_dfs):
+        count_df.columns = pd.MultiIndex.from_product(
+            [[filename], count_df.columns], names=["file", "coordinate"]
+        )
+        count_df.index.name = "gene"
+
+    counts_df = pd.concat(counts_dfs, copy=False, axis=1, sort=True)
+
+    annotation_dfs = [
+        pd.DataFrame(
+            {"1_1": [1, 0], "2_2": [0, 1], "3_3": [0, 1], "4_4": [1, 0]},
+            index=["aar1", "aar2"],
+        ),
+        pd.DataFrame(
+            {
+                "8_8": [1, 0],
+                "7_7": [0, 1],
+                "6_6": [0, 1],
+                "5_5": [1, 0],
+                "11_1": [1, 0],
+                "11_0": [0, 1],
+                "11_-1": [0, 1],
+                "11_-2": [1, 0],
+            },
+            index=["aar1", "aar2"],
+        ),
+    ]
+
+    for filename, annotation_df in zip(annotation_files, annotation_dfs):
+        annotation_df.columns = pd.MultiIndex.from_product(
+            [[filename], annotation_df.columns], names=["file", "coordinate"]
+        )
+        annotation_df.index.name = "aar"
+
+    annotations_df = pd.concat(annotation_dfs, copy=False, axis=1, sort=True)
+
+    return process_input_data(
+        metadata_df,
+        counts_df,
+        annotations_df,
+        num_levels=1,
+        min_total_count=1,
+        min_num_spots_per_slide=1,
+        num_of_neighbors=2,
+        separate_overlapping_tissue_sections=False,
+        min_num_spots_per_tissue_section=1,
+    )
+
+
 @pytest.mark.parametrize(
     "test_input", [(0, 2, 0, 2), (-1, 1, 0, 1), (0, 0.2, -0.5, 0.2)]
 )
@@ -898,6 +986,124 @@ def test_register(splotchinputdata_registration: SplotchInputData) -> None:
     key = random.PRNGKey(0)
     key, key_ = random.split(key, 2)
     register(key_, splotchinputdata_registration, num_steps=10_000)
+    assert all(
+        col in splotchinputdata_registration.metadata.columns
+        for col in ["x_registration", "y_registration"]
+    )
     assert np.all(
         np.isclose(splotchinputdata_registration.metadata.y_registration, 0, atol=1e-2)
     )
+
+
+@pytest.mark.parametrize(
+    "map_method",
+    ["map", "vmap", "pmap"],
+)
+def test_run_nuts(
+    splotchinputdata_inference: SplotchInputData, map_method: str
+) -> None:
+    """Test run_nuts()."""
+    key = random.PRNGKey(0)
+    key, key_ = random.split(key, 2)
+    splotch_result_nuts_batch_1 = run_nuts(
+        key_,
+        ["g1"],
+        splotchinputdata_inference,
+        map_method=map_method,
+        num_warmup=10,
+        num_samples=10,
+        num_chains=4,
+    )
+    assert splotch_result_nuts_batch_1.genes == ["g1"]
+    assert splotch_result_nuts_batch_1.posterior_samples["lambda"].shape == (
+        1,
+        40,
+        12,
+    )
+
+    key, key_ = random.split(key, 2)
+    splotch_result_nuts_batch_2 = run_nuts(
+        key_,
+        ["g2"],
+        splotchinputdata_inference,
+        map_method=map_method,
+        num_warmup=10,
+        num_samples=10,
+        num_chains=4,
+    )
+    assert splotch_result_nuts_batch_2.genes == ["g2"]
+    assert splotch_result_nuts_batch_2.posterior_samples["lambda"].shape == (
+        1,
+        40,
+        12,
+    )
+
+    splotch_result_nuts = splotch_result_nuts_batch_1 + splotch_result_nuts_batch_2
+    assert splotch_result_nuts.genes == ["g1", "g2"]
+    assert splotch_result_nuts.posterior_samples["lambda"].shape == (
+        2,
+        40,
+        12,
+    )
+    assert list(splotch_result_nuts.inference_metrics["summary"]["lambda"].columns) == [
+        "mean",
+        "std",
+        "median",
+        "5.0%",
+        "95.0%",
+        "n_eff",
+        "r_hat",
+    ]
+
+
+@pytest.mark.parametrize(
+    "map_method",
+    ["map", "vmap", "pmap"],
+)
+def test_run_svi(splotchinputdata_inference: SplotchInputData, map_method: str) -> None:
+    """Test run_svi()."""
+    key = random.PRNGKey(0)
+    key, key_ = random.split(key, 2)
+    splotch_result_svi_batch_1 = run_svi(
+        key_,
+        ["g1"],
+        splotchinputdata_inference,
+        map_method=map_method,
+        num_steps=50,
+        num_samples=10,
+    )
+    assert splotch_result_svi_batch_1.genes == ["g1"]
+    assert splotch_result_svi_batch_1.posterior_samples["lambda"].shape == (
+        1,
+        10,
+        12,
+    )
+
+    key, key_ = random.split(key, 2)
+    splotch_result_svi_batch_2 = run_svi(
+        key_,
+        ["g2"],
+        splotchinputdata_inference,
+        map_method=map_method,
+        num_steps=50,
+        num_samples=10,
+    )
+    assert splotch_result_svi_batch_2.genes == ["g2"]
+    assert splotch_result_svi_batch_2.posterior_samples["lambda"].shape == (
+        1,
+        10,
+        12,
+    )
+
+    splotch_result_svi = splotch_result_svi_batch_1 + splotch_result_svi_batch_2
+    assert splotch_result_svi.genes == ["g1", "g2"]
+    assert splotch_result_svi.posterior_samples["lambda"].shape == (
+        2,
+        10,
+        12,
+    )
+    assert all(
+        col in splotch_result_svi.inference_metrics for col in ["losses", "params"]
+    )
+
+    assert splotch_result_svi.inference_metrics["losses"].shape == (2, 50)
