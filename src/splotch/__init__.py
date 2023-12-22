74be6edd16a6eda96f952bf67f9b15eecdf2d56c
Tarmo Äijö <506537+tare@users.noreply.github.com>
2023-12-22 06:42:04 -0500

    Revert "Graph based tissue section detection"

--- src/splotch/__init__.py
+++
@@ -1,7 +0,0 @@
-"""__init__.py."""
-
-from splotch.inference import run_nuts, run_svi
-from splotch.registration import register
-from splotch.utils import get_input_data, savagedickey
-
-__all__ = ["get_input_data", "run_nuts", "run_svi", "register", "savagedickey"]
