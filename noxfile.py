4723c37039ea372d58c06294aee197517c865d13
Tarmo Äijö <506537+tare@users.noreply.github.com>
2025-03-11 09:02:17 -0400

    Update noxfile.py
    - actually add Python 3.13
    - consider "highest" and "lowest-direct" for resolution

--- noxfile.py
+++ noxfile.py
@@ -5,18 +5,19 @@ import nox
 nox.options.default_venv_backend = "uv"
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.10"])
-def tests(session: nox.Session) -> None:
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+@nox.parametrize("resolution", ["highest", "lowest-direct"])
+def tests(session: nox.Session, resolution: str) -> None:
     """Test session."""
     session.install("pytest~=8.3", "pytest-cov~=5.0")
-    session.install(".")
+    session.install(".", "--resolution", resolution)
     session.run("python", "-m", "pytest", *session.posargs)
 
 
 @nox.session(default=False)
 def coverage(session: nox.Session) -> None:
     """Coverage session."""
-    tests(session)
+    tests(session, "highest")
     session.install("pytest-cov~=5.0")
     session.run("coverage", "html")
     session.run("coverage", "report")
